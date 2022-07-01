9e4d8cf589ae7ea6ee33c2d19e1fe29866e8bd10
STEVE MUTINDA <STEVE MUTINDA@MUTINDA19>
2022-07-01 14:43:39 +0300

    Add project files.

---
+++ StackImplementationInterview/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImplementationInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a stack
+            // Using Stack class
+            Stack myStack = new Stack();
+
+            // Adding elements in the Stack Using Push method
+            myStack.Push(10);
+            myStack.Push(20);
+            myStack.Push(30);
+            myStack.Push(40);
+
+            myStack.PrintStack();
+            myStack.Peek();          
+            Console.WriteLine("Item popped from Stack : {0}", myStack.Pop());
+            myStack.PrintStack();
+        }
+    }
+}
---
+++ StackImplementationInterview/Stack.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackImplementationInterview
+{
+    class Stack
+    {
+
+        private const int MaxSize = 1000;
+        private object[] _stack = new object[MaxSize];
+        private int _elementIndex = -1;
+        public Stack()
+        {
+        }
+        public void Push(object data)
+        {
+            if (_elementIndex >= MaxSize - 1)
+            {
+                throw new InvalidOperationException("Stack is full");
+            }
+            _elementIndex++;
+            _stack[_elementIndex] = data;
+        }
+        public object Pop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("No elements in stack");
+            }
+            object element = _stack[_elementIndex];
+            _stack[_elementIndex] = null;
+            _elementIndex--;
+            return element;
+        }
+        public object Peek()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("No elements in stack");
+            }
+            return _stack[_elementIndex];
+        }
+        public bool IsEmpty()
+        {
+            if (_elementIndex < 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        internal void PrintStack()
+        {
+            if (_elementIndex < 0)
+            {
+                Console.WriteLine("Stack Underflow");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Items in the Stack are :");
+                for (int i = _elementIndex; i >= 0; i--)
+                {
+                    Console.WriteLine(_stack[i]);
+                }
+            }
+        }
+    }
+    
+}
